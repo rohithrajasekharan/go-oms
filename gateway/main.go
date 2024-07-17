f9444496b733ee7824b9ad3d85727dd1405ab629
rohithrajasekharan <rohithrajasekharan@outlook.com>
2024-07-17 19:52:23 +0530

    env setup inside common

--- gateway/main.go
+++ gateway/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+
+	_ "github.com/joho/godotenv/autoload"
+	common "github.com/rohithrajasekharan/commons"
 )
 
-const (
-	httpAddr = ":8080"
+var (
+	httpAddr = common.EnvString("HTTP_ADDR", ":8080")
 )
 
 func main() {
