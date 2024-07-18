9c08e639d8d4f68ae05cf9bc7c10fbe8d70d9726
rohithrajasekharan <rohithrajasekharan@outlook.com>
2024-07-18 15:53:30 +0530

    grpc client on gateway and create order

--- gateway/main.go
+++ gateway/main.go
@@ -6,15 +6,29 @@ import (
 
 	_ "github.com/joho/godotenv/autoload"
 	common "github.com/rohithrajasekharan/commons"
+	pb "github.com/rohithrajasekharan/commons/api"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var (
-	httpAddr = common.EnvString("HTTP_ADDR", ":8080")
+	httpAddr         = common.EnvString("HTTP_ADDR", ":8080")
+	orderServiceAddr = "localhost:2000"
 )
 
 func main() {
+	conn, err := grpc.NewClient(orderServiceAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to dial server: %v", err)
+	}
+	defer conn.Close()
+
+	log.Println("Dialing orders service at:", orderServiceAddr)
+
+	c := pb.NewOrderServiceClient(conn)
+
 	mux := http.NewServeMux()
-	handler := NewHandler()
+	handler := NewHandler(c)
 	handler.RegisterRoutes(mux)
 
 	log.Printf("Starting http server at %s", httpAddr)
