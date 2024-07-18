24d6cda2fbb407fec852c2caa7ec60ab40e67583
rohithrajasekharan <rohithrajasekharan@outlook.com>
2024-07-18 15:52:11 +0530

    grpc handler for orders

--- orders/main.go
+++ orders/main.go
@@ -1,9 +1,35 @@
 package main
 
-import "context"
+import (
+	"context"
+	"log"
+	"net"
+
+	common "github.com/rohithrajasekharan/commons"
+	"google.golang.org/grpc"
+)
+
+var (
+	grpcAddr = common.EnvString("GRPC_ADDR", "localhost:2000")
+)
 
 func main() {
+
+	grpcServer := grpc.NewServer()
+
+	l, err := net.Listen("tcp", grpcAddr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	defer l.Close()
+
 	store := NewStore()
 	svc := NewService(store)
+	NewGRPCHandler(grpcServer, svc)
 	svc.CreateOrder(context.Background())
+
+	log.Println("GRPC server started at", grpcAddr)
+	if err := grpcServer.Serve(l); err != nil {
+		log.Fatal(err.Error())
+	}
 }
